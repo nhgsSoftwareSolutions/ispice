419962fa3b26995f2aec04e97552d00931ec7b97
Danzibob <Danzibob@gmail.com>
2016-08-11 20:28:25 +0100

    Experimenting with locking scrolling
    This makes the only way for the user to get around to use the navbar, hence showing off th fancy transition and making the page line up properly

--- scripts/ispice.js
+++ scripts/ispice.js
@@ -8,6 +8,13 @@ $(document).scroll(function() {
   }
 });
 
+$('body').on({
+    'mousewheel': function(e) {
+        if (e.target.id == 'el') return;
+        e.preventDefault();
+        e.stopPropagation();
+    }
+})
 
 var recentScroll = false;
 $(document).scroll(function() {
